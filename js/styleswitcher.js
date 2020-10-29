639501c8e48996d0737d9ac87cef5d2c30bb3a0e
Sohel Rana <73012378+ShohelRana56@users.noreply.github.com>
2020-10-29 11:31:53 +0600

    My protfolio website

---
+++ js/styleswitcher.js
@@ -0,0 +1,32 @@
+const links = document.querySelectorAll(".alternate-style"),
+    totalLinks = links.length;
+
+
+function setActiveStyle(color) {
+    for (let i = 0; i < totalLinks; i++) {
+        if (color === links[i].getAttribute("title")) {
+            links[i].removeAttribute("disabled");
+        } else {
+            links[i].setAttribute("disabled", "true")
+        }
+
+    }
+}
+// Body Skin
+const bodySkin=document.querySelectorAll(".body-skin"),
+    totalbodySkin=bodySkin.length,
+    styleSwitcher = document.querySelector(".style-switcher");
+
+    for(let i=0; i<totalbodySkin; i++){
+        bodySkin[i].addEventListener("change", function(){
+           if(this.value == "dark"){
+               document.body.className="dark";
+           }else{
+               document.body.className=" ";
+           }
+        })
+    }
+document.querySelector(".toggler-style-switcher").addEventListener("click", () => {
+    styleSwitcher.classList.toggle("open");
+})
+
